fef5d2a9f5d4e809fde48abf61f1d34f761c5b3d
nabil-otsmane <hn_otsmane@esi.dz>
2021-06-14 23:46:21 +0100

    random

--- app/src/main/java/com/clovertech/autolibodb/MainActivity.kt
+++ app/src/main/java/com/clovertech/autolibodb/MainActivity.kt
@@ -1,26 +1,88 @@
 package com.clovertech.autolibodb
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import com.clovertech.autolibodb.ui.SharedViewModel
+import io.socket.client.IO
+import io.socket.client.Socket
+import io.socket.emitter.Emitter
+import org.json.JSONObject
+import java.lang.Exception
+import java.net.URISyntaxException
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: SharedViewModel
+
+    private lateinit var mSocket: Socket
+
+    private val onError: Emitter.Listener = Emitter.Listener {
+        this@MainActivity.runOnUiThread(Runnable {
+            try {
+                val data: Exception = it[0] as Exception
+                Toast.makeText(this@MainActivity, data.message, Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                val data: JSONObject = it[0] as JSONObject
+
+            }
+
+        })
+    }
+    private val onLink: Emitter.Listener = Emitter.Listener {
+        this@MainActivity.runOnUiThread(Runnable {
+            val data: JSONObject = it[0] as JSONObject
+
+            println("linking ...")
+        })
+    }
+
+    private val onDisconnect: Emitter.Listener = Emitter.Listener {
+        this@MainActivity.runOnUiThread(Runnable {
+            findNavController(R.id.fragmentContainer).navigate(R.id.navigation_loading)
+        })
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        viewModel =
+            ViewModelProvider(this).get(SharedViewModel::class.java)
+
+
+        try {
+            val opts = IO.Options()
+            opts.port = 8000
+            opts.path = "/socket"
+
+
+            mSocket = IO.socket("http://192.168.43.222:8123", opts)
+        } catch(e: URISyntaxException) {
+            e.printStackTrace()
+        }
+
+        mSocket.on("error", onError)
+        mSocket.on("connect_error", onError)
+        mSocket.on("start link", onLink)
+        mSocket.on("disconnect", onDisconnect)
+        mSocket.connect()
+
+    }
+
+    fun getSocket(): Socket {
+        return mSocket
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        mSocket.disconnect()
+        mSocket.off("error", onError)
+        mSocket.off("connect_error", onError)
+        mSocket.off("start link", onLink)
     }
 }
